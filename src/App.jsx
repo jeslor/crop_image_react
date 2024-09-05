6b3bd9d8d5a40dee03dc5ef6b8a41f4d433a5256
twani <jbestius@gmail.com>
2024-09-05 07:41:31 +0200

    adjust the colors of the cropped image background

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ const App = () => {
       <ImageForm handleSetFile={handleSetFile} handleSetImage={handleSetImage}/>
       {
         images.length &&(
-          <div className="w-full pt-7">
-            <h2 className="pl-8 font-semibold text-2xl">Cropped Images</h2>
+          <div className="w-full pt-7 bg-[rgba(0,0,0,0.8)] rounded-t-3xl">
+            <h2 className="pl-8 font-semibold text-2xl text-slate-100">Cropped Images</h2>
             <div className="flex px-8 flex-wrap gap-4 pb-8 pt-4">
               {images.map((image, index) => (
                 <img key={index} src={image} alt="cropped" className="h-[150px] w-[150px] rounded-md"/>
